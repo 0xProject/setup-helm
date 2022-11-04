3b8ffdc4db3e3cfc13ac736c780357defed90a1c
Marcin Wolny <marcin@0xproject.com>
2022-11-04 15:26:33 +0100

    Install helm binary

--- src/helm.ts
+++ src/helm.ts
@@ -1,13 +1,19 @@
 import * as tc from '@actions/tool-cache';
+import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+import * as filepath from 'path';
 
 export interface Options {
   version: string;
-  checksum: string;
 }
 
 export async function install(options: Options): Promise<number> {
-  const path = tc.downloadTool(url);
-  const extracted = tc.extract7z(path);
+  const path = await tc.downloadTool(
+    `https://get.helm.sh/helm-v${options.version}-linux-amd64.tar.gz`
+  );
+  const extracted = await tc.extract7z(path);
 
-  return await exec.exec('go', args);
+  core.addPath(filepath.join(extracted));
+
+  return exec.exec('helm', ['version']);
 }
